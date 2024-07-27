f4c0f515313eb426617d34bfba499f9a9951f439
TomasSilvaValencia <tsilvaval@gmail.com>
2024-07-27 11:45:12 -0500

    Create Algoritmo Para Conocernos.py

---
+++ Algoritmo Para Conocernos.py
@@ -0,0 +1,4 @@
+#¡Tarea!
+import random
+data = ["Mi afición favorita es hacer juegos de mesa", "Mi plato favorito es la ensalada", "Me siento orgullosos de mis avances en la programación", "Tengo la mascota mas mona del mundo"]
+print(random.choice(data))
